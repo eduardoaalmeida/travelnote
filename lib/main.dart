7b4a83b9d27e165239afd8b23a1c6966622e9d48
adrianofonseca711 <adriano.sfonseca@souunit.com.br>
2026-05-18 12:07:27 -0300

    pequenas alteracoes na main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'carregamento_page.dart';
+import 'agenda_page.dart';
+import 'editar_perfil_page.dart';
+import 'configuracoes_page.dart';
+import 'alterar_dados_page.dart';
 
 void main() {
   runApp(const MeuApp());
@@ -14,6 +19,12 @@ class MeuApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.lightBlueAccent),
       home: const CarregamentoPage(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('pt', 'BR')],
     );
   }
 }
